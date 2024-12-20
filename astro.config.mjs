8c926c709b133273ade8107b56c6eb89f76955d4
Devon Cash <devon.p.cash@gmail.com>
2024-12-19 19:21:53 -0800

    Added adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
+
   integrations: [
     starlight({
       title: "devon.cash",
       customCss: ["./src/styles/custom.css"],
       plugins: [],
       components: {
-        "MarkdownContent": "./src/components/CommentablePage.astro",
-      }
+        MarkdownContent: "./src/components/CommentablePage.astro",
+      },
     }),
   ],
+
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  }),
 });
