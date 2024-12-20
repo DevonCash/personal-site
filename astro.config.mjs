9da5b787776059a5a809f368acaad16606d307a8
Devon Cash <devon.p.cash@gmail.com>
2024-12-19 20:39:54 -0800

    Custom Sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,10 +14,40 @@ export default defineConfig({
       components: {
         MarkdownContent: "./src/components/CommentablePage.astro",
       },
+      sidebar: [
+        {
+          label: "Rulesets",
+          collapsed: false,
+          autogenerate: { directory: "Rulesets" },
+        },
+        {
+          label: "Modules",
+          collapsed: false,
+          items: [
+            {
+              label: "LANCER: Hardsuits",
+              slug: "modules/lancer-hardsuits"
+            },
+            {
+              label: "LANCER: Scarcity",
+              slug: "modules/lancer-scarcity"
+            },
+            {
+              label: "WWN: Eberron",
+              slug: "modules/worlds-without-number-eberron"
+            },
+            {
+              label: "The Luminous Order",
+              collapsed: true,
+              autogenerate: { directory: "Modules/The Luminous Order" }
+            }
+          ]
+        }
+      ],
     }),
   ],
 
   adapter: cloudflare({
-    imageService: 'cloudflare'
+    imageService: "cloudflare",
   }),
 });
